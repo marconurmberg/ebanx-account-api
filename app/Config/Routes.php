f74ed6fa8883e04a632fa8ec37c1e14a2fca2f40
marconurmberg <marconurmberg69@gmail.com>
2025-04-18 17:53:02 -0300

    feat: app config; routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+$routes->post('/reset', 'StateController::resetState');
+$routes->post('/event', 'Event::index');
+$routes->get('/balance', 'AccountController::getAccountBalance');
