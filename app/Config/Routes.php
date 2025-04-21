bd987a2b0afd2f1074365720c2f97ebb5a5df4f7
Marco Nurmberg <marconurmberg69@gmail.com>
2025-04-21 15:28:28 -0300

    refactor: removing index route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-
-
 $routes->post('/reset', 'StateController::resetState');
 $routes->post('/event', 'AccountController::handleAccountOperationEvent');
 $routes->get('/balance', 'AccountController::getAccountBalance');
