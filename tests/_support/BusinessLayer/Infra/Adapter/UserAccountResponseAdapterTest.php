3f26d1129d27356249939d47bbdecee787a8b091
Marco Nurmberg <marconurmberg69@gmail.com>
2025-04-21 14:41:33 -0300

    feat: adding response adapter missing test cases

--- tests/_support/BusinessLayer/Infra/Adapter/UserAccountResponseAdapterTest.php
+++ tests/_support/BusinessLayer/Infra/Adapter/UserAccountResponseAdapterTest.php
@@ -9,6 +9,15 @@ use PHPUnit\Framework\TestCase;
 
 class UserAccountResponseAdapterTest extends TestCase
 {
+    private UserAccountResponseAdapter $adapter;
+    private ResponseDTO $responseDTO;
+
+    protected function setUp(): void
+    {
+        $this->adapter = new UserAccountResponseAdapter();
+        $this->responseDTO = new ResponseDTO();
+    }
+
     /** @dataProvider balanceResponseDataProvider */
     public function testFromUserAccountEntityToResponseDTO($initialBalance, $expectedBalance): void
     {
@@ -21,9 +30,88 @@ class UserAccountResponseAdapterTest extends TestCase
         ];
         $expectedStatus = 201;
 
-        $adapter = new UserAccountResponseAdapter();
-        $responseDTO = new ResponseDTO();
-        $result = $adapter->fromUserAccountEntityToResponseDTO($userAccountEntity, $responseDTO);
+        $result = $this->adapter->fromUserAccountEntityToResponseDTO($userAccountEntity, $this->responseDTO);
+
+        $this->assertEquals($expectedBody, $result->getBody());
+        $this->assertEquals($expectedStatus, $result->getHttpStatus());
+    }
+
+    /** @dataProvider depositEventDataProvider */
+    public function testFromDepositEventUserAccountEntityToResponseDTO($initialBalance, $expectedBalance): void
+    {
+        $accountId = 1;
+        $userAccountEntity = $this->createUserAccountEntity($accountId, $initialBalance);
+
+        $expectedBody = [
+            "destination" => [
+                "id" => (string)$accountId,
+                "balance" => $expectedBalance
+            ]
+        ];
+        $expectedStatus = 201;
+
+        $result = $this->adapter->fromDepositEventUserAccountEntityToResponseDTO(
+            $userAccountEntity,
+            $this->responseDTO
+        );
+
+        $this->assertEquals($expectedBody, $result->getBody());
+        $this->assertEquals($expectedStatus, $result->getHttpStatus());
+    }
+
+    /** @dataProvider withdrawEventDataProvider */
+    public function testFromWithdrawEventUserAccountEntityToResponseDTO($initialBalance, $expectedBalance): void
+    {
+        $accountId = 1;
+        $userAccountEntity = $this->createUserAccountEntity($accountId, $initialBalance);
+
+        $expectedBody = [
+            "origin" => [
+                "id" => (string)$accountId,
+                "balance" => $expectedBalance
+            ]
+        ];
+        $expectedStatus = 201;
+
+        $result = $this->adapter->fromWithdrawEventUserAccountEntityToResponseDTO(
+            $userAccountEntity,
+            $this->responseDTO
+        );
+
+        $this->assertEquals($expectedBody, $result->getBody());
+        $this->assertEquals($expectedStatus, $result->getHttpStatus());
+    }
+
+    /** @dataProvider transferEventDataProvider */
+    public function testFromTransferEventAccountEntitiesToResponseDTO(
+        $originBalance,
+        $destinationBalance,
+        $expectedOriginBalance,
+        $expectedDestinationBalance
+    ): void {
+        $originAccountId = 1;
+        $destinationAccountId = 2;
+        
+        $originAccount = $this->createUserAccountEntity($originAccountId, $originBalance);
+        $destinationAccount = $this->createUserAccountEntity($destinationAccountId, $destinationBalance);
+
+        $expectedBody = [
+            "origin" => [
+                "id" => (string)$originAccountId,
+                "balance" => $expectedOriginBalance
+            ],
+            "destination" => [
+                "id" => (string)$destinationAccountId,
+                "balance" => $expectedDestinationBalance
+            ]
+        ];
+        $expectedStatus = 201;
+
+        $result = $this->adapter->fromTransferEventAccountEntitiesToResponseDTO(
+            $originAccount,
+            $destinationAccount,
+            $this->responseDTO
+        );
 
         $this->assertEquals($expectedBody, $result->getBody());
         $this->assertEquals($expectedStatus, $result->getHttpStatus());
@@ -46,6 +134,46 @@ class UserAccountResponseAdapterTest extends TestCase
             "ZERO_BALANCE" => [0, 0.00],
             "ROUND_UP" => [100.995, 101.00],
             "ROUND_DOWN" => [100.994, 100.99],
+        ];
+    }
+
+    public static function depositEventDataProvider(): array
+    {
+        return [
+            "DEPOSIT_INTEGER" => [100, 100.00],
+            "DEPOSIT_DECIMAL" => [150.75, 150.75],
+            "DEPOSIT_MULTIPLE_DECIMALS" => [200.12345, 200.12],
+            "DEPOSIT_ROUND_UP" => [100.995, 101.00],
+            "DEPOSIT_ROUND_DOWN" => [100.994, 100.99]
+        ];
+    }
+
+    public static function withdrawEventDataProvider(): array
+    {
+        return [
+            "WITHDRAW_INTEGER" => [100, 100.00],
+            "WITHDRAW_DECIMAL" => [150.75, 150.75],
+            "WITHDRAW_MULTIPLE_DECIMALS" => [200.12345, 200.12],
+            "WITHDRAW_ROUND_UP" => [100.995, 101.00],
+            "WITHDRAW_ROUND_DOWN" => [100.994, 100.99]
+        ];
+    }
+
+    public static function transferEventDataProvider(): array
+    {
+        return [
+            "TRANSFER_INTEGER" => [
+                100, 50, 100.00, 50.00
+            ],
+            "TRANSFER_DECIMAL" => [
+                150.75, 200.25, 150.75, 200.25
+            ],
+            "TRANSFER_MULTIPLE_DECIMALS" => [
+                200.12345, 300.98765, 200.12, 300.99
+            ],
+            "TRANSFER_ROUND_BOTH" => [
+                100.995, 200.994, 101.00, 200.99
+            ]
         ];
     }
 }
